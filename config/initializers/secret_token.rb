d5441b6b2ef439109cc1dea08cc1a38b921369b0
Helena Freire <helenamcfreire@gmail.com>
2013-11-28 14:22:03 -0200

    Baby Cam, first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babycam::Application.config.secret_token = '5a9f573a71ef35fa8b76bce5fb457d6f3b86446940ecb3fa1e64e7b2a163c29861ed7d6d3611eac7ba14088077d1266c167640ca1985017477c02e05cb9429c5'
